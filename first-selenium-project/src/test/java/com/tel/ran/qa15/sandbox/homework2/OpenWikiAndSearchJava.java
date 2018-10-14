d882bd4caf475fd3406064e2a1838672da6430aa
Akulapera <porokhnia.anastasiya@gmail.com>
2018-10-14 17:44:21 +0300

    homework1 and 2 in right repozitiry

---
+++ first-selenium-project/src/test/java/com/tel/ran/qa15/sandbox/homework2/OpenWikiAndSearchJava.java
@@ -0,0 +1,23 @@
+package com.tel.ran.qa15.sandbox.homework2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class OpenWikiAndSearchJava extends TestBase{
+
+    @Test
+    public void openSiteTest() {
+
+        enterToWiki();
+
+        writeJavaInFild();
+
+        pressButton();
+    }
+
+}
